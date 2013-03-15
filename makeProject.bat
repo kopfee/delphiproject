c095e57b6facb74e81a3f43e6f99d69901ccec73
macson <ksgmacson@gmail.com>
2013-03-15 15:25:25 +0800

    更新各个项目的信息

--- makeProject.bat
+++ makeProject.bat
@@ -1,10 +1,15 @@
 @echo off
-echo "please input a new project name ..."
+set str0=abcdefghijklmnopqrstuvwxyz0123456789
+:re
+set /p str1="please input a new project name :"
+echo %str1%|findstr "[^^%str0%]">nul&&(echo 请重新输入&goto :re)
+rem echo %str1%
+mkdir %str1%
 
-mkdir bin
-mkdir dcu
-mkdir doc
-mkdir src
-mkdir dll
-mkdir log
-mkdir config
+mkdir %str1%\bin
+mkdir %str1%\dcu
+mkdir %str1%\doc
+mkdir %str1%\src
+mkdir %str1%\dll
+mkdir %str1%\log
+mkdir %str1%\config
